5badc803ebec680f73562079aac21bdfc2ca0dbe
unknown <etornham@gmail.com>
2024-12-30 20:32:02 +0100

    Initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="text-white text-4xl bg-violet-500 p-6">
+        ViNG
+      </div>
+      
+    </>
+    
+  );
+}
+
+export default App;
