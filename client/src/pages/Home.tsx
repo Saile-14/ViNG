c0f88bda38de865567c737ee8cb0c9fb9beeea93
Saile-14 <186436741+Saile-14@users.noreply.github.com>
2025-01-02 22:59:20 +0100

    added two express api endpoints, tried addind prettier,eslint. Tailwindcss lost functionality, shadcn might work

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Home () {
+
+    
     return (<p className="p-6 bg-violet-300 text-white text-2xl">Home!</p>)
 }
